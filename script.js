f1059ec22f9d4eb93a7921e7b2d5961002dd4efd
isaque ferreira de sousa <isaqueferreiradesousa@gmail.com>
2022-07-29 15:03:11 -0300

    refinando ui

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function construtor_tarefas() {
         status: false,
         indice: listaTarefas.length,
     }
+    showButton_deleteall()
     listaTarefas.push(obj)
     // console.log(listaTarefas)
 }
@@ -27,22 +28,23 @@ const adicionar = () => {
         alert("Escreva uma tarefa.")
         return "";
 
-    }
-    else {
+    } else {
 
         construtor_tarefas()
 
         input.value = "";
-        
+
         let tela = document.getElementById('tarefas')
         tela.innerHTML = ''
         save()
 
         loud()
 
+        showButton_deleteall()
+
         mostrar()
 
-       
+
 
     }
 
@@ -50,109 +52,101 @@ const adicionar = () => {
 
 function render(tarefas) {
 
-    
+
     let item = document.createElement("div");
     item.classList.add("tarefa");
     // item.id = tarefa.titulo
-    
+
     item.innerHTML = `
         <label class="label">
 
             <input type="checkbox" style="width: 50px;" id="${tarefas.titulos}" class="check" ${tarefas.status?"checked":""} datas-set${tarefas.indice}>
-            <div style="color: #FCA311; font-size: 1.5rem;">${tarefas.titulos}</div>
+            <div style="color: #03113a; font-size: 1.5rem;">${tarefas.titulos}</div>
 
         </label>
-        <button style="background-color: #FCA311; color: black;" onclick="remove_item(${tarefas.titulo})" datas-set${tarefas.indice}>x</button>
+        <button style="background-color: #6C2600;cursor: pointer;" onclick="remove_item(${tarefas.titulo})" datas-set${tarefas.indice}>x</button>
         
     `
     // element .checked = tarefa.status;
-    
+
+    showButton_deleteall()
     document.getElementById("tarefas").appendChild(item);
 
-    
-    
+
+
 }
 
-function limpar_tela(){
-    let todolist= document.getElementById("tarefas")
-    while(todolist.firstChild){
+function limpar_tela() {
+    let todolist = document.getElementById("tarefas")
+    while (todolist.firstChild) {
         todolist.removeChild(todolist.lastChild)
     }
 }
 
 
-function mostrar(){
-    
-    if(listaTarefas.length <= 0){
-        
+function mostrar() {
+
+    if (listaTarefas.length <= 0) {
+
         return ''
-    }
-    else{
+    } else {
         limpar_tela()
         let tela = document.getElementById('tarefas')
         tela.innerHTML = ''
         listaTarefas.forEach(render)
+        showButton_deleteall()
 
     }
 }
 
-function loud(){
+function loud() {
     var local_storage = localStorage.getItem("dado");
     var nome = JSON.parse(local_storage)
     console.log(nome)
-   
+    showButton_deleteall()
+
 
     listaTarefas = nome;
 }
 
-function save(){
+function save() {
 
     let listastring = JSON.stringify(listaTarefas)
     // console.log(listastring)
-    localStorage.setItem("dado",listastring)    
+    showButton_deleteall()
+    localStorage.setItem("dado", listastring)
 }
 
 
 window.onload = function recarregar() {
 
     if (localStorage.getItem("dado") == undefined) {
-       save()
+        save()
     }
 
     loud()
 
+    showButton_deleteall()
+
+
     mostrar()
 
     let exemplo = document.querySelectorAll(".tarefa")
-    
+
 }
 
 
 
 function remove_item(elemento) {
 
-
-
-    // var index = listaTarefas.indexOf(tarefas.titulos);
     listaTarefas.splice(elemento, 1)
     save()
     loud()
     let tela = document.getElementById('tarefas')
     tela.innerHTML = ''
+    showButton_deleteall()
     mostrar()
 
-
-    // // listaTarefas.forEach((tarefa, index)=>{
-    // //     if( item === tarefa.titulo){
-    // //         var index = listaTarefas.indexOf(tarefa.titulo)
-    // //         listaTarefas.splice(index, 1)
-    // //         save()
-    // //         loud()
-    // //         
-    // //         mostrar()
-    // //     }
-        
-    // })
     localStorage.setItem("dado", resultado.innerHTML)
 
 }
@@ -163,16 +157,31 @@ function resetarr() {
     if (input == "sim") {
         localStorage.setItem("dado", "")
         var nome = localStorage.getItem("dado")
+        listaTarefas = [];
         resultado.innerHTML = nome;
+        showButton_deleteall()
 
     }
 
+
 }
 
+function showButton_deleteall() {
+
+    const button = document.getElementById("remover_tudo")
+
+
+    if (listaTarefas.length <= 2) {
+
+        button.style.display = "none"
+
+    }
+    if (listaTarefas.length >= 2) {
+
+        button.style.display = "flex"
+
+    }
 
 
 
-
-
-
-
+}
